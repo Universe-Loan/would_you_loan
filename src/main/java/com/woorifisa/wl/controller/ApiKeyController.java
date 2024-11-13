890c75fc1a626bf4cfa7277f85bef14c741366ba
eunhyea <eunhye.go.092@gmail.com>
2024-11-13 10:40:46 +0900

    hiding api key

---
+++ src/main/java/com/woorifisa/wl/controller/ApiKeyController.java
@@ -0,0 +1,16 @@
+package com.woorifisa.wl.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ApiKeyController {
+    @Value("${API_KEY_DATA_EH}")
+    private String apiKey;
+
+    @GetMapping("/api/key")
+    public String getApiKey() {
+        return apiKey;
+    }
+}
