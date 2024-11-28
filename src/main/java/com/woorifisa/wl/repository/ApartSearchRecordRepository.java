8ad5894dff2c47ed75dae8dd3f4fabc42d8e39ce
DoxB <jg.doxb@gmail.com>
2024-11-28 16:21:37 +0900

    WF-124 report-recent-db connect

--- src/main/java/com/woorifisa/wl/repository/ApartSearchRecordRepository.java
+++ src/main/java/com/woorifisa/wl/repository/ApartSearchRecordRepository.java
@@ -3,6 +3,12 @@ package com.woorifisa.wl.repository;
 import com.woorifisa.wl.model.entity.ApartSearchRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ApartSearchRecordRepository extends JpaRepository<ApartSearchRecord, Long> {
+import java.util.List;
 
+public interface ApartSearchRecordRepository extends JpaRepository<ApartSearchRecord, Long> {
+    List<ApartSearchRecord> findByUserIdOrderByUrlIdDesc(Long userId);
+    void deleteByUrlIdAndUserId(Long urlId, Long userId);
+
+    // 추가: 같은 user_id와 url로 검색
+    boolean existsByUserIdAndUrl(Long userId, String url);
 }
